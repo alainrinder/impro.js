625759857097e53142bbd9bcf2e89c1e1403c26b
alainrinder <alain.rinder@gmail.com>
2018-03-10 00:03:12 +0100

    First commit
    gitignore, gruntfile, package.json, readme & logos

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        banner: '\'use strict\';\n\n',
+        separator: '\n\n'
+      },
+      dist: {
+        src: ['src/*.js', 'src/**/*.js'],
+        dest: 'bin/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          '<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/*.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        undef: true,
+        unused: 'vars',
+        latedef: true,
+        eqeqeq: true,
+        quotmark: 'single',
+        nonbsp: true,
+        predef: ['gimel'],
+        globals: {
+          window: true,
+          console: true,
+          module: true,
+          document: true,
+          gimel: true,
+        }
+      }
+    },
+    removeLoggingCalls: {
+      files: ['<%= pkg.name %>.js'],
+      options: {
+        methods: ['assert', 'log', 'info', 'warn'], 
+        strategy: function(statement) {
+          return 'null /* ' + statement + ' */';
+        }
+      }
+    },
+    jsdoc: {
+      dist : {
+        src: ['src/*.js', 'src/**/*.js'], 
+        dest: 'doc',
+        lenient: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-remove-logging-calls');
+  grunt.loadNpmTasks('grunt-jsdoc');
+
+  grunt.registerTask('build', ['jshint', 'concat', 'removeLoggingCalls', 'uglify', 'jsdoc']);
+  grunt.registerTask('debug', ['jshint', 'concat']);
+};
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+![logo](https://github.com/alainrinder/impro.js/raw/master/media/logo/logo.h.svg "Logo")
+
 # impro.js
-JavaScript IMage PROcessing library &amp; web app
+**Im**age **Pro**cessing Javascript Library &amp; Web App
+
+## Authors
+* [Alain Rinder](https://github.com/alainrinder)
+* [Jean-Daniel Borowy](https://github.com/jdborowy)
